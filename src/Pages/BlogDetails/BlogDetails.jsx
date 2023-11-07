231d2a9ed701d0e1a9c41aaf986fd726c7022599
Fariha1865 <farihaamin187@gmail.com>
2023-11-07 21:05:50 +0600

    Comment input box hidden for blog owner

--- src/Pages/BlogDetails/BlogDetails.jsx
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -33,11 +33,14 @@ const BlogDetails = () => {
 
     console.log(blog)
     return (
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-7xl mx-auto">
+            <div className="flex justify-center mt-5">
+                <h1 className='button2 font-mono text-blue-700 shadow-lg shadow-blue-500 md:mt-40 lg:mt-0 text-4xl lg:text-2xl font-bold text-center'>{blog?.title}</h1>
+            </div>
             <div className="p-10">
-                <div className="flex justify-between gap-10">
-                    <div className="w-2/4 rounded overflow-hidden shadow-lg shadow-blue-700 p-10">
-                        <img className="w-full h-72" src={blog?.image} alt="Card Image" />
+                <div className="flex justify-between gap-10 items-center w-[1024px">
+                    <div className="w-[4000px] h-[450px] rounded overflow-hidden shadow-lg shadow-blue-700 p-10 ml-20 mt-10">
+                        <img className="w-full h-32" src={blog?.image} alt="Card Image" />
 
 
                         <div className="flex justify-center">
@@ -45,19 +48,31 @@ const BlogDetails = () => {
                         </div>
 
                         <div className="px-6 py-4">
-                            <h1 className="text-center"><span className="font-bold">Blog By: </span>{blog?.userName}</h1>
+                            <h1 className="text-center grays"><span className="font-bold">Blog By: </span>{blog?.userName}</h1>
                             <h1 className="text-center mt-3 mb-5"><span className="font-bold">Published On: </span>{blog?.dateTime?.split("T")[0]}</h1>
-                            <div className="font-bold text-xl mb-2 text-center">{blog?.title}</div>
+                            <div className="font-bold text-sm mb-2 text-center">{blog?.title}</div>
 
                         </div>
                     </div>
+
                     <div>
-                        <CommentBox></CommentBox>
+                        <hr className="border-solid border-2 border-blue-500 shadow-blue-900 shadow-2xl mt-10" />
+                        <div>
+                            <p className="max-h-80 overflow-y-auto mt-10 mb-10 bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 text-white text-xl p-4 rounded-lg shadow-2xl">
+                                {blog?.long}
+                            </p>
+
+                        </div>
+                        <hr className="border-solid border-2 border-blue-500 shadow-blue-900 shadow-2xl mt-10" />
                     </div>
 
-                </div>
-            </div>
 
+                </div>
+
+            </div>
+            <div className="mb-10">
+                <CommentBox blog={blog}></CommentBox>
+            </div>
 
 
         </div>
--- src/Pages/BlogDetails/CommentBox.jsx
+++ src/Pages/BlogDetails/CommentBox.jsx
@@ -1,18 +1,19 @@
 import { Button } from "flowbite-react";
 import "./details.css"
+import PropTypes from 'prop-types';
 import { useContext, useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../hooks/UseAxiosSecure";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Comment from "./Comment";
-const CommentBox = () => {
+const CommentBox = ({ blog }) => {
 
     const { user } = useContext(AuthContext);
     const axiosSecure = useAxiosSecure();
     const [comments, setComments] = useState([]);
-    const [newComment,setNewComment] = useState(false);
+    const [newComment, setNewComment] = useState(false);
 
-    const commentsUrl = '/getComments'
+    const commentsUrl = `/getComments/${blog?._id}`
     useEffect(() => {
         axiosSecure.get(commentsUrl)
             .then(data => {
@@ -28,7 +29,7 @@ const CommentBox = () => {
 
                 }
             })
-    }, [newComment,axiosSecure])
+    }, [newComment, axiosSecure, commentsUrl])
 
     const handleComment = () => {
 
@@ -38,8 +39,9 @@ const CommentBox = () => {
         const userName = user?.displayName;
         const { dateTime } = { dateTime: new Date() };
         const userImage = user?.photoURL;
+        const id = blog?._id;
 
-        const commentData = { userMail, userName, userImage, comment, dateTime };
+        const commentData = { userMail, userName, userImage, comment, dateTime, id };
 
 
         const url = '/comments';
@@ -62,23 +64,30 @@ const CommentBox = () => {
     comments.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
     return (
         <div>
-            <div className="flex items-center">
-                <div className="inputs-container w-96 mt-10 border-b-4 border-x-blue-900">
-                    <input className="inputs w-96 h-40" name="text" type="text" id="comment" placeholder="Add a Comment" />
-                    <label className="label">Type Comment</label>
-                    <div className="topline"></div>
-                    <div className="underline"></div>
-                </div>
-                <Button className="mt-10 h-12 rounded-l-none" onClick={handleComment}>Publish Comment</Button>
-            </div>
+            {
+                user?.email !== blog?.userMail ? <div className="flex items-center">
+                    <div className="inputs-container w-96 mt-10 border-b-4 border-x-blue-900">
+                        <input className="inputs w-96 h-40" name="text" type="text" id="comment" placeholder="Add a Comment" />
+                        <label className="label">Type Comment</label>
+                        <div className="topline"></div>
+                        <div className="underline"></div>
+                    </div>
+                    <Button className="mt-10 h-12 rounded-l-none" onClick={handleComment}>Publish Comment</Button>
+                </div> : <div/>
+              
+             
+            }
 
             <div className=" bg-blue-200 p-4 rounded-lg h-96 overflow-y-auto">
-            {
-                comments?.map(comment => <Comment key={comment._id} commentEach={comment}></Comment>)
-            }
+                {
+                    comments?.map(comment => <Comment key={comment._id} commentEach={comment}></Comment>)
+                }
             </div>
         </div>
     );
 };
 
+CommentBox.propTypes={
+    blog:PropTypes.object.isRequired
+}
 export default CommentBox;
