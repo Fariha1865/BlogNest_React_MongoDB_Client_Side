2142805037a7b1b1470ee65025d1c0cf219e276f
Fariha1865 <farihaamin187@gmail.com>
2023-11-08 03:12:19 +0600

    react fast marqury scroller addded

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import NewsLetter from "./NewsLetter/NewsLetter";
 import RecentBlogs from "./Recent/RecentBlogs";
 import { motion } from 'framer-motion';
 import DrawerContact from "./DrawerContact";
+import Marquee from "react-fast-marquee";
 
 
 const Home = () => {
@@ -27,14 +28,19 @@ const Home = () => {
                 )}
             </InView>}
 
-            <div className="flex flex-col md:flex-row items-center p-20 gap-5 my-20">
+            <div className="mb-20 mt-32">
+            <Marquee pauseOnHover speed={70}>
+                <p className="text-xl font-bold">For proper blog writing tips, Visit this documentation: <a href="https://www.shoutmeloud.com/blogging-tips.html" target="__blank" className="text-blue-800 underline">Blog Writing Tips</a> or Contact Us </p>
+            </Marquee>
+            <div className="flex flex-col md:flex-row items-center p-20 gap-5">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ml4mQJjgX6g" frameBorder="0" allowfullscreen className="w-1/2"></iframe>
 
 
-                <h1 className="text-2xl font-bold">For more personalized tips<br/> and blog writing suggestions,<br></br>please contact us with your queries..</h1>
+                <h1 className="text-2xl font-bold">For more personalized tips<br /> and blog writing suggestions,<br></br>please contact us with your queries..</h1>
                 <DrawerContact></DrawerContact>
 
             </div>
+        </div>
         </div>
     );
 };
